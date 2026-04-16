cac71b444f478c6ca7fb0cde746c1c96e56e6209
Vrumugun <wisniewski1@gmail.com>
2026-04-16 19:22:20 +0200

    add shell dummy

---
+++ time_triggered_system_1/src/sapl/sapl-shell.adb
@@ -0,0 +1,15 @@
+package body SAPL.Shell is
+
+   procedure Initialize is
+   begin
+      --  Shell initialization logic goes here.
+      null;
+   end Initialize;
+
+   procedure Update is
+   begin
+      --  Shell logic goes here.
+      null;
+   end Update;
+
+end SAPL.Shell;
