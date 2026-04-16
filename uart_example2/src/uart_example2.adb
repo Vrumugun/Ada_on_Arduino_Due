5f36650131447cdad4c30fb69362955a42b5445f
Vrumugun <wisniewski1@gmail.com>
2026-04-16 09:07:00 +0200

    update comment

--- uart_example2/src/uart_example2.adb
+++ uart_example2/src/uart_example2.adb
@@ -64,7 +64,7 @@ begin
          Clock_Frequency     => 84_000_000);
 
    --  The watchdog is enabled by default on the SAM3X8E.
-   --  If not reset or disabled, it will reset the board after ~16 seconds.
+   --  If not reloaded or disabled, it will reset the board after ~16 seconds.
    --  Configure watchdog or call WDT.Disable instead
    WDT.Configure (16#FFF#);
 
